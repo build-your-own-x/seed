a19ed6a65a7aa31e5f1433de26a9cd792c19b53e
techzealot <techzealot@foxmail.com>
2022-07-13 18:40:33 +0800

    update List impl

--- src/main/java/com/techzealot/collection/list/ArrayList.java
+++ src/main/java/com/techzealot/collection/list/ArrayList.java
@@ -1,6 +1,14 @@
 package com.techzealot.collection.list;
 
-public class ArrayList {
+import java.io.Serializable;
+import java.util.RandomAccess;
+
+/**
+ * 实现 增删改查 序列化优化 泛型支持 随机访问
+ *
+ * @param <E>
+ */
+public class ArrayList<E> implements RandomAccess, Serializable {
 
     public boolean isEmpty() {
         return true;
