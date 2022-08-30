ba886d4ea90842049aec8d0e8a3b1eb54c407770
techzealot <techzealot@foxmail.com>
2022-08-30 18:48:21 +0800

    二分搜索树初步

--- src/main/java/com/techzealot/collection/map/MyTreeMap.java
+++ src/main/java/com/techzealot/collection/map/MyTreeMap.java
@@ -1,4 +1,24 @@
 package com.techzealot.collection.map;
 
-public class MyTreeMap {
+import java.util.Comparator;
+
+/**
+ *
+ */
+public class MyTreeMap<K, V> {
+
+    /**
+     * 实现自定义compare方法统一比较的逻辑消除重复代码
+     *
+     * @param comparator
+     * @param o1
+     * @param o2
+     * @return
+     */
+    private static int compare(Comparator comparator, Object o1, Object o2) {
+        if (comparator != null) {
+            return comparator.compare(o1, o2);
+        }
+        return ((Comparable) o1).compareTo(o2);
+    }
 }
--- src/main/java/com/techzealot/collection/tree/BST.java
+++ src/main/java/com/techzealot/collection/tree/BST.java
@@ -1,4 +1,41 @@
 package com.techzealot.collection.tree;
 
-public class BST {
+/**
+ * 二分搜索树
+ */
+public class BST<E extends Comparable<E>> {
+    private Node root;
+    private int size;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean add(E e) {
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        return true;
+    }
+
+    public boolean contains(Object o) {
+        return true;
+    }
+
+    private class Node {
+        E e;
+        Node left;
+        Node right;
+
+        public Node(E e) {
+            this.left = null;
+            this.e = e;
+            this.right = null;
+        }
+    }
 }
