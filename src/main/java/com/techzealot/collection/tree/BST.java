88fd055a27d5fd00e237033481d2617b827cf4fd
techzealot <techzealot@foxmail.com>
2022-09-02 15:59:02 +0800

    实现二分搜索树基本功能
    1.查询最大值最小值、删除最大最小值
    2.删除任意元素
    3.实现层序遍历

--- src/main/java/com/techzealot/collection/tree/BST.java
+++ src/main/java/com/techzealot/collection/tree/BST.java
@@ -2,10 +2,18 @@ package com.techzealot.collection.tree;
 
 import lombok.NonNull;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
 import java.util.function.Consumer;
 
 /**
- * 二分搜索树:该实现不包含重复元素
+ * 二分搜索树:该实现不包含重复元素,不支持null元素
+ * <p>
+ * 二叉树递归技巧：返回当前根节点，并把null视作空树，Node代表子树
+ * <p>
+ * 标准库中处理集合对象比较问题的方式：
+ * 若定义比较器则使用比较器，若未定义比较器则元素本身必须为Comparable否则强转异常
  */
 public class BST<E extends Comparable<E>> {
     private Node root;
@@ -23,9 +31,9 @@ public class BST<E extends Comparable<E>> {
      * @param e
      * @return false if e already exists
      */
-    public boolean add(E e) {
+    public boolean add(@NonNull E e) {
         Boolean modified = false;
-        add(root, e, modified);
+        root = add(root, e, modified);
         return modified;
     }
 
@@ -38,10 +46,14 @@ public class BST<E extends Comparable<E>> {
         return add0(root, e);
     }
 
+    public boolean addNoRecursive(@NonNull E e) {
+        return true;
+    }
+
     /**
      * 优化：空节点视作空树
      * 递归实现插入节点
-     * 返回当前遍历的根节点或新插入节点
+     * 返回当前子树的根节点
      *
      * @param node
      * @param e
@@ -89,8 +101,44 @@ public class BST<E extends Comparable<E>> {
         return false;
     }
 
+    /**
+     * 用被删节点左子树的最大值或右子树的最小值替换被删节点
+     *
+     * @param o
+     * @return
+     */
     public boolean remove(Object o) {
-        return true;
+        if (o == null || !(o instanceof Comparable)) return false;
+        if (size == 0) return false;
+        Boolean removed = false;
+        root = remove(root, o, removed);
+        return removed;
+    }
+
+    private Node remove(Node node, Object o, Boolean removed) {
+        if (node == null) return null;
+        Comparable x = (Comparable) o;
+        if (x.compareTo(node.e) > 0) {
+            node.right = remove(node.right, o, removed);
+            return node;
+        } else if (x.compareTo(node.e) < 0) {
+            node.left = remove(node.left, o, removed);
+            return node;
+        } else {
+            removed = true;
+            if (node.right == null) {
+                Node left = node.left;
+                node.left = null;
+                size--;
+                return left;
+            } else {
+                Node rightMin = minimum(node.right);
+                rightMin.right = removeMin(node.right);
+                rightMin.left = node.left;
+                node.right = node.left = null;
+                return rightMin;
+            }
+        }
     }
 
     public boolean contains(Object o) {
@@ -104,18 +152,51 @@ public class BST<E extends Comparable<E>> {
         if (node == null) {
             return false;
         }
-        Comparable x = (Comparable) o;
-        if (x.compareTo(node.e) == 0) {
-            return true;
-        } else if (x.compareTo(node.e) < 0) {
-            return contains(node.left, o);
+        if (o instanceof Comparable x) {
+            if (x.compareTo(node.e) == 0) {
+                return true;
+            } else if (x.compareTo(node.e) < 0) {
+                return contains(node.left, o);
+            } else {
+                return contains(node.right, o);
+            }
         } else {
-            return contains(node.right, o);
+            return false;
         }
     }
 
     /**
+     * 前序遍历非递归实现 使用栈实现
+     *
+     * @param action
+     */
+    public void preOrderNR(Consumer<? super E> action) {
+        if (root == null) return;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            action.accept(current.e);
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
+    }
+
+    public void inOrderNR(Consumer<? super E> action) {
+
+    }
+
+    public void postOrderNR(Consumer<? super E> action) {
+
+    }
+
+    /**
      * 前序遍历：根节点->左子树->右子树
+     * 深度优先遍历
      *
      * @param action
      */
@@ -166,6 +247,96 @@ public class BST<E extends Comparable<E>> {
         postOrder(node.right, action);
         action.accept(node.e);
     }
+
+    /**
+     * 层序遍历
+     *
+     * @param action
+     */
+    public void levelOrder(Consumer<? super E> action) {
+        if (root == null) return;
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            action.accept(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    public E minimum() {
+        if (size == 0) {
+            return null;
+        }
+        return minimum(root).e;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    public E removeMin() {
+        E ret = minimum();
+        if (ret == null) return null;
+        root = removeMin(root);
+        return ret;
+    }
+
+    /**
+     * 返回当前子树的根节点
+     *
+     * @param node
+     * @return
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            size--;
+            node.right = null;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    public E maximum() {
+        if (size == 0) return null;
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node) {
+        if (node.right == null) return node;
+        return maximum(node.right);
+    }
+
+    public E removeMax() {
+        if (size == 0) return null;
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    //todo
+    // predecessor,successor,floor,ceil,rank,select,
 
     private class Node {
         E e;
